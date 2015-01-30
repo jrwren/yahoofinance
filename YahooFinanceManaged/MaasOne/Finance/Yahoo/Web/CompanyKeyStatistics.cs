54494551997d2f72835151301875036e5686eb1d
MaasOne <maasone@live.com>
2015-01-30 20:39:46 +0100

    master 0.12.0.1 alpha

--- YahooFinanceManaged/MaasOne/Finance/Yahoo/Web/CompanyKeyStatistics.cs
+++ YahooFinanceManaged/MaasOne/Finance/Yahoo/Web/CompanyKeyStatistics.cs
@@ -1,4 +1,22 @@
-﻿using System;
+﻿// **************************************************************************************************
+// **  
+// **  Yahoo! Finance Managed
+// **  Written by Marius Häusler 2015
+// **  It would be pleasant, if you could contact me when you are using this code.
+// **  Contact: maasone@live.com
+// **  Project Home: https://yahoofinance.codeplex.com/
+// **  
+// **************************************************************************************************
+// **  
+// **  Copyright @ Marius Häusler
+// **  
+// **  Licensed under GNU Lesser General Public License (LGPL) (Version 2.1, February 1999).
+// **  
+// **  License Text: https://yahoofinance.codeplex.com/license
+// **  
+// **  
+// **************************************************************************************************
+using System;
 using System.Net;
 using System.Collections.Generic;
 using Newtonsoft.Json;
--- YahooFinanceManaged/System/Xml/Linq/XObject.cs
+++ YahooFinanceManaged/System/Xml/Linq/XObject.cs
@@ -1,4 +1,22 @@
-﻿#if (NET20)
+﻿// **************************************************************************************************
+// **  
+// **  Yahoo! Finance Managed
+// **  Written by Marius Häusler 2015
+// **  It would be pleasant, if you could contact me when you are using this code.
+// **  Contact: maasone@live.com
+// **  Project Home: https://yahoofinance.codeplex.com/
+// **  
+// **************************************************************************************************
+// **  
+// **  Copyright @ Marius Häusler
+// **  
+// **  Licensed under GNU Lesser General Public License (LGPL) (Version 2.1, February 1999).
+// **  
+// **  License Text: https://yahoofinance.codeplex.com/license
+// **  
+// **  
+// **************************************************************************************************
+#if (NET20)
 using System;
 using System.Collections.Generic;
 using System.Xml;
