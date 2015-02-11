610deb39570e59f676da5a13a035dc882656f4c9
MaasOne <maasone@live.com>
2015-02-11 04:15:53 +0100

    master 0.12.0.15

--- YahooFinanceManaged.UnitTest/Properties/AssemblyInfo.cs
+++ YahooFinanceManaged.UnitTest/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // Allgemeine Informationen über eine Assembly werden über folgende 
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("MaasOne.UnitTest")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Yahoo! Finance Managed Unit Test")]
+[assembly: AssemblyDescription("Unit Test for Yahoo! Finance Managed")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MaasOne.UnitTest")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCompany("MaasOne")]
+[assembly: AssemblyProduct("Yahoo! Finance Managed Unit Test for .NET 4.5")]
+[assembly: AssemblyCopyright("Copyright © Marius Häusler 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- YahooFinanceManaged/System/Extensions2.cs
+++ YahooFinanceManaged/System/Extensions2.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿#region "License"
+// *********************************************************************************************
+// **                                                                                         **
+// **  Yahoo! Finance Managed                                                                 **
+// **                                                                                         **
+// **  Copyright (c) Marius Häusler 2009-2015                                                 **
+// **                                                                                         **
+// **  Licensed under GNU Lesser General Public License (LGPL) (Version 2.1, February 1999).  **
+// **                                                                                         **
+// **  License: https://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt                        **
+// **                                                                                         **
+// **  Project: https://yahoofinance.codeplex.com/                                            **
+// **                                                                                         **
+// **  Contact: maasone@live.com                                                              **
+// **                                                                                         **
+// *********************************************************************************************
+#endregion
+using System;
 
 namespace System
 {
